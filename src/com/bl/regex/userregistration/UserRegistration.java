f8d5742166523272074b287e9d01417a30570141
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 20:09:44 +0530

    Added: Check Predefined Mobile Number

--- src/com/bl/regex/userregistration/UserRegistration.java
+++ src/com/bl/regex/userregistration/UserRegistration.java
@@ -56,6 +56,23 @@ public class UserRegistration {
 			System.out.println(email + " is an Invalid Email address");
 	}
 
+	public static void checkValidMobileNumber(String mobileNumber) {
+
+		boolean isMobileNumber;
+		String emailRegex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+		Pattern patternObject = Pattern.compile(emailRegex);
+		if (mobileNumber == null) {
+			isMobileNumber = false;
+		}
+		Matcher matcherObject = patternObject.matcher(mobileNumber);
+		isMobileNumber = matcherObject.matches();
+
+		if (isMobileNumber)
+			System.out.println(mobileNumber + " is a Valid  Mobile Number\n");
+		else
+			System.out.println(mobileNumber + " is an Invalid Mobile Number");
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("---------- Welcome To User Registration ----------");
@@ -72,6 +89,10 @@ public class UserRegistration {
 		System.out.println("Enter Your Email Address");
 		String email = scannerObject.next();
 		checkValidEmail(email);
+
+		System.out.println("Enter Your Mobile Number");
+		String mobileNumber = scannerObject.nextLine();
+		checkValidMobileNumber(mobileNumber);
 
 		scannerObject.close();
 
