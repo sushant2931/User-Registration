f694351b1959f140b98b704d2088ce780b44216e
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 19:50:01 +0530

    Added: Check User Name Valid or Not Valid

--- src/com/bl/regex/userregistration/UserRegistration.java
+++ src/com/bl/regex/userregistration/UserRegistration.java
@@ -1,7 +1,36 @@
 package com.bl.regex.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	public static void checkValidFirstName(String firstName) {
+
+		boolean isFirstName;
+		String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
+		Pattern patternObject = Pattern.compile(firstNameRegex);
+		if (firstName == null) {
+			isFirstName = false;
+		}
+		Matcher matcherObject = patternObject.matcher(firstName);
+		isFirstName = matcherObject.matches();
+
+		if (isFirstName)
+			System.out.println(firstName + " is an Valid First Name");
+		else
+			System.err.println(firstName + " is an Invalid First Name");
+}
+
 	public static void main(String[] args) {
+
 		System.out.println("---------- Welcome To User Registration ----------");
+		Scanner scannerObject = new Scanner(System.in);
+
+		System.out.println("Enter Your First Name");
+		String firstName = scannerObject.next();
+		checkValidFirstName(firstName);
+
+		scannerObject.close();
 	}
 }
