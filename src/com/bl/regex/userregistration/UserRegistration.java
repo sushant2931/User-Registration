aaf3481e42d53722673c1f9f73e2c41e829475b3
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 20:31:26 +0530

    Added:Adding Special Character

--- src/com/bl/regex/userregistration/UserRegistration.java
+++ src/com/bl/regex/userregistration/UserRegistration.java
@@ -76,7 +76,7 @@ public class UserRegistration {
 	public static void checkValidPassword(String password) {
 
 		boolean isPassword;
-		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]*([@#$%^&-+=()])*).{8,}$";
+		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$";
 		Pattern patternObject = Pattern.compile(passwordRegex);
 		if (password == null) {
 			isPassword = false;
@@ -88,6 +88,7 @@ public class UserRegistration {
 			System.out.println(password + " is a Valid  Password\n");
 		else
 			System.out.println(password + " is an Invalid Password");
+
 	}
 
 	public static void main(String[] args) {
