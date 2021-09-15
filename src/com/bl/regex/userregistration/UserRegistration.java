070298257c4aecb985c0cfb40e9f1dea4f916c4f
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 19:42:22 +0530

    Added: Welcome In User Registration Program

---
+++ src/com/bl/regex/userregistration/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.bl.regex.userregistration;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		System.out.println("---------- Welcome To User Registration ----------");
+	}
+}
