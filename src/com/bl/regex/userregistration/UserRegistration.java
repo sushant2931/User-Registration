a55dc5f644027768f1e86be60606b72a7fe37f42
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 20:27:38 +0530

    Added: Adding 1 Numeric In Password

--- src/com/bl/regex/userregistration/UserRegistration.java
+++ src/com/bl/regex/userregistration/UserRegistration.java
@@ -76,7 +76,7 @@ public class UserRegistration {
 	public static void checkValidPassword(String password) {
 
 		boolean isPassword;
-		String passwordRegex = "^(?=.*[A-Z])([a-zA-Z0-9]*([@#$%^&-+=()])*).{8,}$";
+		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]*([@#$%^&-+=()])*).{8,}$";
 		Pattern patternObject = Pattern.compile(passwordRegex);
 		if (password == null) {
 			isPassword = false;
