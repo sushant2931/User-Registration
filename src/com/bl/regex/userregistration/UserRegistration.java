648690e9d0832c2f22cdf7cb3e16a849c2d62876
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 19:55:49 +0530

    Added: Check Last Name Valid or Not

--- src/com/bl/regex/userregistration/UserRegistration.java
+++ src/com/bl/regex/userregistration/UserRegistration.java
@@ -20,7 +20,24 @@ public class UserRegistration {
 			System.out.println(firstName + " is an Valid First Name");
 		else
 			System.err.println(firstName + " is an Invalid First Name");
-}
+	}
+
+	public static void checkValidLastName(String lastName) {
+
+		boolean isLastName;
+		String lastNameRegex = "[A-Z]{1}[a-z]{2,}";
+		Pattern patternObject = Pattern.compile(lastNameRegex);
+		if (lastName == null) {
+			isLastName = false;
+		}
+		Matcher matcherObject = patternObject.matcher(lastName);
+		isLastName = matcherObject.matches();
+
+		if (isLastName)
+			System.out.println(lastName + " is an Valid Last Name");
+		else
+			System.err.println(lastName + " is an Invalid Last Name");
+	}
 
 	public static void main(String[] args) {
 
@@ -30,6 +47,10 @@ public class UserRegistration {
 		System.out.println("Enter Your First Name");
 		String firstName = scannerObject.next();
 		checkValidFirstName(firstName);
+
+		System.out.println("Enter Your Last Name");
+		String lastName = scannerObject.next();
+		checkValidLastName(lastName);
 
 		scannerObject.close();
 	}
