dc9f02dafd60a275e6b3a5fb336a212170ac78a7
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 20:03:12 +0530

    Added: Check Email Valid Or Not Valid

--- src/com/bl/regex/userregistration/UserRegistration.java
+++ src/com/bl/regex/userregistration/UserRegistration.java
@@ -39,6 +39,23 @@ public class UserRegistration {
 			System.err.println(lastName + " is an Invalid Last Name");
 	}
 
+	public static void checkValidEmail(String email) {
+
+		boolean isEmail;
+		String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+		Pattern patternObject = Pattern.compile(emailRegex);
+		if (email == null) {
+			isEmail = false;
+		}
+		Matcher matcherObject = patternObject.matcher(email);
+		isEmail = matcherObject.matches();
+
+		if (isEmail)
+			System.out.println(email + " is a Valid Email address\n");
+		else
+			System.out.println(email + " is an Invalid Email address");
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("---------- Welcome To User Registration ----------");
@@ -52,6 +69,11 @@ public class UserRegistration {
 		String lastName = scannerObject.next();
 		checkValidLastName(lastName);
 
+		System.out.println("Enter Your Email Address");
+		String email = scannerObject.next();
+		checkValidEmail(email);
+
 		scannerObject.close();
+
 	}
 }
